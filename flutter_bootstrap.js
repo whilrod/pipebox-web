85290fc184c5d5043f8091ecb34b7535014d6fc2
whilrod <whilrod@gmail.com>
2026-05-29 17:47:14 -0500

    fix: añadir estado mute. Release v 0.1.10

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "272184388" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "789381628" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
